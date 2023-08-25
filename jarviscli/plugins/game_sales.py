2057fabfa0de9729430da1432498997405edc3c3
Jiajun Xu <jiajunxu2@gmail.com>
2023-08-24 21:45:26 -0400

    added input and url

--- jarviscli/plugins/game_sales.py
+++ jarviscli/plugins/game_sales.py
@@ -1,4 +1,14 @@
 import requests
+from plugin import plugin
 
 @plugin("sale")
 def get_sale(jarvis, s):
+    jarvis.say("Enter the game you would like to search: ")
+    game = jarvis.input()
+    url = "https://www.cheapshark.com/api/1.0/games?title={game}"
+
+    payload={}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    
